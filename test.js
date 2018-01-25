ebfcd63d500f47c230af9150ec91b619f1024d7d
Seongbo <jewi@ajou.ac.kr>
2018-01-25 22:50:27 +0900

    Poloneix add ticker not yet

--- test.js
+++ test.js
@@ -1,34 +1,63 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/market'); // 기본 설정에 따라 포트가 상이 할 수 있습니다.
+var MongoClient = require('mongodb').MongoClient;
+var request = require('request');
+var cron = require('node-cron');
+const Poloniex = require('poloniex-api-node');
+let poloniex = new Poloniex();
+mongoose.connect('mongodb://localhost:27017/test');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
 	console.log("mongo db connection OK.");
 });
-
-var testSchema = mongoose.Schema({
-	name: String
+var tradeSite = new mongoose.Schema({
+	"Sname" : {type : String, unique : true}
 });
-
-testSchema.methods.speak = function () {
-	var greeting = this.name
-	? "Meow name is " + this.name
-	: "I don't have a name"
-	console.log("speak() - " + greeting);
-}
-
-var TestModel = mongoose.model("BTC_XMR", testSchema);
-
-var testIns = new TestModel({ name: "BTC_XMR"});
-
-testIns.save(function(err, testIns){
-	if(err) return console.error(err);
-	testIns.speak();
+var market = new mongoose.Schema({
+	"Sname" : {type : String, ref : 'sitename'},
+	"Mname" : {type : String, unique : true}
 });
-
-TestModel.find(function(err, models){
-	if(err) return console.error(err);
-	console.log("find() - "+models);
+var poloniexData = new mongoose.Schema({
+	"Sname" : {type : String, ref : 'sitename'},
+	"Mname" : {type : String, ref : 'marketname'},
+	"date" : Date,
+	"last" : Number,
+	"lowestAsk" : Number,
+	"highestBid" : Number,
+	"percentChange" : Number,
+	"baseVolume" : Number,
+	"quoteVolume" : Number,
+	"isFrozen" : Number,
+	"24hrHigh" : Number,
+	"24hrLow" : Number
 });
-
-TestModel.find({name:/^testIns/});	
+var sitename = mongoose.model('sitename', tradeSite, 'sitename');
+var marketname = mongoose.model('marketname', market, 'marketname');
+// Poloniex
+var PoloniexSite = new sitename({Sname : 'Poloniex'});
+PoloniexSite.save(function (err){
+	//if(err) return handleError(err);
+})
+var PoloniexMarket = new marketname({Sname : PoloniexSite.Sname, Mname : 'usdt_btc'});
+PoloniexMarket.save(function (err){
+	//if(err) return handleError(err);
+})
+poloniex.subscribe('ticker');
+poloniex.on('message', (channelName, data, seq) => {
+  if (channelName === 'ticker') {
+    console.log(`Ticker: ${JSON.stringify(data)}`);
+	if (data.currencyPair === 'usdt_btc'){
+	}    
+  }
+ 
+});
+poloniex.on('open', () => {
+  console.log(`Poloniex WebSocket connection open`);
+});
+poloniex.on('close', (reason, details) => {
+  console.log(`Poloniex WebSocket connection disconnected`);
+});
+poloniex.on('error', (error) => {
+  console.log(`An error has occured`);
+});
+poloniex.openWebSocket({ version: 2 });
